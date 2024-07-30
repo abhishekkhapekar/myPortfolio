064722abece61537a44f5ff848fca4adb0446659
Abhishek Khapekar <abhishekkhapekar64@gmail.com>
2024-07-30 13:08:38 +0530

    My First changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Greet from './component/Greet';
+
+function App() {
+  return (
+    <div className="App">
+         <Greet></Greet>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Greet.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+
+class Greet extends Component{
+    render(){
+        return (
+           <h1 class="centered">Website is in progress</h1>
+        );
+    }
+}
+
+export default Greet;
