8872710fcd0513b888ee6ccf556245b0e3342654
Abhishek Khapekar <abhishekkhapekar64@gmail.com>
2025-02-06 00:02:30 +0530

    Add wrangler file

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
+import {ThemeProvider} from 'styled-components'
 
 import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
-    <>
-      <GlobalStyle></GlobalStyle>
+    // <>
+      <ThemeProvider theme={{}}>
+      <GlobalStyle></GlobalStyle>  
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
-    </>
+      </ThemeProvider>
+    // </>
   )
 }
 
