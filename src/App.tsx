9462ad20c7e4ae011c5ee9e152429d0788e72a53
Abhishek Khapekar <abhishekkhapekar64@gmail.com>
2025-02-06 00:11:44 +0530

    Add themeprovider 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
 import {ThemeProvider} from 'styled-components'
+import {myTheme} from './components/theme'
 
 import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     // <>
-      <ThemeProvider theme={{}}>
+      <ThemeProvider theme={{myTheme}}>
       <GlobalStyle></GlobalStyle>  
       <Header></Header>
       <Main></Main>
---
+++ src/components/theme.js
@@ -0,0 +1,10 @@
+export const myTheme = {
+    colors: {
+      primary: '#007bff',
+      secondary: '#6c757d',
+      background: '#f8f9fa',
+    },
+    fonts: {
+      body: 'sans-serif',
+    },
+  };
