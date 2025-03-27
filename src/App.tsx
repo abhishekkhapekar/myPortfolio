dbeb5f392cba735a2c7ff53d12ad1460b503e1e1
Abhishek Khapekar <abhishekkhapekar64@gmail.com>
2025-03-27 13:53:23 +0530

    Changes in code for new deployment

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,16 @@ import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
-import {ThemeProvider} from 'styled-components'
-import {myTheme} from './components/theme'
 
 import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
-    // <>
-      <ThemeProvider theme={{myTheme}}>
-      <GlobalStyle></GlobalStyle>  
+    <>
+      <GlobalStyle></GlobalStyle>
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
-      </ThemeProvider>
-    // </>
+    </>
   )
 }
 
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,9 +9,9 @@ import instagramIcon from '../../assets/instagram.svg'
 export function Footer() {
   return (
     <Container className="footer">
-      <a href="https://abhishekkhapekar.pages.dev" className="logo">
-        <span>www.abhishek</span>
-        <span>dk.in</span>
+      <a href="https://vinayaksingh.in" className="logo">
+        <span>www.vinayak</span>
+        <span>singh.in</span>
       </a>
       <div>
         <p>
@@ -21,35 +21,35 @@ export function Footer() {
       </div>
       <div className="social-media">
         <a
-          href="https://www.linkedin.com/in/abhishek-khapekar"
+          href="https://www.linkedin.com/in/codevinayak"
           target="_blank"
           rel="noreferrer"
         >
           <img src={linkedin} alt="Linkedin" />
         </a>
         <a
-          href="https://github.com/abhishekkhapekar"
+          href="https://github.com/CodeVinayak/"
           target="_blank"
           rel="noreferrer"
         >
           <img src={githubIcon} alt="GitHub" />
         </a>
         <a
-          href="https://api.whatsapp.com/send/?phone=%2B919763832117&text=Hello+Abhishek"
+          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak"
           target="_blank"
           rel="noreferrer"
         >
           <img src={whatsapp} alt="Whatsapp" />
         </a>
         <a
-          href="https://t.me/yellow_flash_abhi"
+          href="https://t.me/CodeVinayak"
           target="_blank"
           rel="noreferrer"
         >
           <img src={telegram} alt="telegram" />
         </a>
         <a
-          href="https://www.instagram.com/abhishekkhapekar64/"
+          href="https://www.instagram.com/vinayaksingh.in"
           target="_blank"
           rel="noreferrer"
         >
